7495e35126fa344a5714c2f1870c04d1a91d61c1
talesOfTolstoy <borjaguardamino3@gmail.com>
2023-04-06 02:51:00 +0200

    messed about with undotree

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,2 +1,19 @@
 -- nnoremap <F5> :UndotreeToggle<CR> turned into:;
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+---- Automatically open undotree
+--vim.cmd([[autocmd UndotreeToggle]])
+--
+---- Set undotree width to 30 columns
+--vim.g.undotree_WindowLayout =  { { relative = 'win', width = 30, height = 15, row = 1, col = 1 } }
+--
+-- Define function to open undotree on the right side of the screen
+--function open_undotree()
+--  vim.cmd('UndotreeToggle')
+--  vim.cmd('wincmd R')
+--end
+--
+---- Open undotree on startup
+--vim.api.nvim_command('UndotreeToggle')
+--vim.api.nvim_command('wincmd R')
+--
