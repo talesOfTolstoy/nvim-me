29a80dd3d227ff5e8d71aeae44b5431d43bdaaed
talesOfTolstoy <borjaguardamino3@gmail.com>
2023-04-06 02:14:26 +0200

    auto added sidebar

--- after/plugin/sidebar.lua
+++ after/plugin/sidebar.lua
@@ -1,7 +1,7 @@
 require("sidebar-nvim").setup({
     disable_default_keybindings = 0,
     bindings = nil,
-    open = false,
+    open = true,
     side = "left",
     initial_width = 35,
     hide_statusline = false,
@@ -15,3 +15,16 @@ require("sidebar-nvim").setup({
     datetime = { format = "%a %b %d, %H:%M", clocks = { { name = "local" } } },
     todos = { ignored_paths = { "~" } },
 })
+
+-- Hide status line in the sidebar window
+vim.api.nvim_command('autocmd FileType sidebar setlocal statusline=%!v:lua.StatusLine()')
+
+function StatusLine()
+  if vim.fn.win_gettype() == 'sidebar' then
+    return ''
+  else
+    -- Set your desired status line format for non-sidebar windows here
+    return '%<%f %h%m%r%=%-14.(%l,%c%V%) %P%=%X'
+  end
+end
+
